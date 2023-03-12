112ce467901fbf21427455c817700fc010ad2199
aggsdp <aggsdpz@gmail.com>
2023-03-12 20:52:54 -0300

    seller class

--- jdbc-project/src/application/Main.java
+++ jdbc-project/src/application/Main.java
@@ -4,15 +4,17 @@ import db.DB;
 import db.DbException;
 import db.DbIntegrityException;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
