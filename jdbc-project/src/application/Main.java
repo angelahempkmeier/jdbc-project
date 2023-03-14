4208f57f2fea93293a799c56508bde94b7bc5551
aggsdp <aggsdpz@gmail.com>
2023-03-13 21:12:11 -0300

    findAll implementation

--- jdbc-project/src/application/Main.java
+++ jdbc-project/src/application/Main.java
@@ -29,5 +29,13 @@ public class Main {
             System.out.println(obj);
         }
 
+        System.out.println();
+        System.out.println("TESTE 3: seller findAll");
+        List<Seller> list2 = sellerDao.findAll();
+        for(Seller obj : list2){
+            System.out.println(obj);
+        }
+
+
     }
 }
