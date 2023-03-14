80a06911726f738d7455861d741c7441e5aaa526
aggsdp <aggsdpz@gmail.com>
2023-03-13 21:27:03 -0300

    insert implementation

--- jdbc-project/src/application/Main.java
+++ jdbc-project/src/application/Main.java
@@ -19,8 +19,9 @@ public class Main {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("TESTE 1: seller findById");
-        Seller seller = sellerDao.findById(3);
-        System.out.println(seller);
+        Seller seller0 = sellerDao.findById(3);
+        System.out.println(seller0);
+
         System.out.println();
         System.out.println("TESTE 2: seller findByDepartment");
         Department department = new Department(2, null);
@@ -35,6 +36,12 @@ public class Main {
         for(Seller obj : list2){
             System.out.println(obj);
         }
+
+        System.out.println();
+        System.out.println("TESTE 4: seller insert");
+        Seller seller1 = new Seller(null, "Greg", "greg@gmail", new Date(), 4000.0, department);
+        sellerDao.insert(seller1);
+        System.out.println("Inserted! New id = " + seller1.getId());
 
 
     }
